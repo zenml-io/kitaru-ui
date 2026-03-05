fb41d6e85f9c321c504fe4b6c019569d8dd26cbd
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-03-05 22:37:35 +0100

    feat: install-zenml script

--- devenv.nix
+++ devenv.nix
@@ -17,6 +17,10 @@
   # env.ZENML_DEBUG = "true";
   env.ZENML_ANALYTICS_OPT_IN = "false";
 
+  scripts.install-zenml-branch.exec = ''
+    sh scripts/install-zenml-branch.sh $1
+  '';
+
   languages.python = {
     enable = true;
     package = pkgs.python312;
