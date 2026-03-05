799223f9fe8e4f5ab49ee1b04c07b75a638bae8a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-03-05 16:19:13 +0100

    feat: vite dev proxy

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,39 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-	test: {
-		exclude: ["node_modules/**/*", "dist/**/*"],
-	},
-	plugins: [
-		tanstackRouter({
-			target: "react",
-			autoCodeSplitting: true,
-		}),
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	const backendUrl = env.VITE_BACKEND_URL;
+
+	return {
+		test: {
+			exclude: ["node_modules/**/*", "dist/**/*"],
+		},
+		server: {
+			proxy: {
+				"/api": {
+					target: backendUrl,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
-		}),
-		tailwindcss(),
-		tsconfigPaths(),
-	],
+		},
+		plugins: [
+			tanstackRouter({
+				target: "react",
+				autoCodeSplitting: true,
+			}),
+			react({
+				babel: {
+					plugins: [["babel-plugin-react-compiler"]],
+				},
+			}),
+			tailwindcss(),
+			tsconfigPaths(),
+		],
+	};
 });
