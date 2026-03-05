d3dfb1814d3f1001ae65d674f5691c7d3c68b994
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-03-05 17:15:59 +0100

    Add Openapi Type Generation & Build Validation (#2)
    * feat: add types
    
    * chore: sort package json
    
    * ci: validation action
    
    * ci: use lts
    
    * feat: vite dev proxy
    
    * chore: adjust linter
    
    * add dummy unit test
    
    ---------
    
    Co-authored-by: Bart Mikolajczuk <bart.mikolajczuk@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-	globalIgnores(["dist"]),
+	globalIgnores(["dist", "src/shared/api/types.ts", "src/routeTree.gen.ts"]),
 	{
 		files: ["**/*.{ts,tsx}"],
 		extends: [
---
+++ scripts/types.js
@@ -0,0 +1,47 @@
+import fs from "node:fs";
+import openapiTS, { astToString } from "openapi-typescript";
+
+// Logging function with emojis
+function log(message, emoji) {
+	console.log(`${emoji} ${message}`);
+}
+
+async function generateTypes(baseUrl) {
+	log("Script started.", "✨");
+
+	const ast = await openapiTS(new URL(`${baseUrl}/openapi.json`), {
+		exportType: true,
+		defaultNonNullable: false,
+		transform: (schema) => {
+			customTransformer(schema);
+		},
+	});
+
+	log("Writing output to file...", "📝");
+	const contents = astToString(ast);
+	fs.writeFileSync("./src/shared/api/types.ts", contents);
+
+	log("Script completed successfully.", "✅");
+}
+
+/**
+ *
+ * @param {import("openapi-typescript").SchemaObject} schema
+ * @returns {import("openapi-typescript").SchemaObject}
+ */
+export function customTransformer(schema) {
+	if (schema.type === "object" && !schema.properties) {
+		schema.additionalProperties = true;
+	}
+	return schema;
+}
+
+// Check if the script is called with at least one argument
+if (process.argv.length < 4) {
+	console.error("Usage: pnpm generate:types -- <baseurl>");
+	process.exit(1);
+}
+
+// Take the base URL from the first argument
+const baseUrl = process.argv[3];
+generateTypes(baseUrl);
