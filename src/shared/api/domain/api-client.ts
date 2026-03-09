cc61fef52cb8e4fcd81e42efccd1515fef219f37
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-03-09 15:13:30 +0100

    feat: add api client (#10)

---
+++ src/shared/api/domain/api-client.ts
@@ -0,0 +1,41 @@
+import createClient, { type Middleware } from "openapi-fetch";
+import { FetchError } from "./fetch-error";
+import type { paths } from "../openapi";
+import { throwFetchErrorFromResponse } from "../utils/throw-fetch-error-from-response";
+
+const defaultHeaders = {
+	"Content-Type": "application/json",
+	"Source-Context": "dashboard-v2",
+};
+
+export const apiClient = createClient<paths>({
+	baseUrl: "",
+	credentials: "include",
+	headers: defaultHeaders,
+});
+
+const errorHandlingMiddleware: Middleware = {
+	async onResponse({ request, response }) {
+		if (!response.ok) {
+			await throwFetchErrorFromResponse({
+				response,
+				url: request.url,
+				method: request.method,
+			});
+		}
+
+		return response;
+	},
+	onError({ error, request }) {
+		throw new FetchError({
+			status: 0,
+			statusText: "REQUEST_FAILED",
+			message: "Request failed before receiving a response",
+			url: request.url,
+			method: request.method,
+			cause: error,
+		});
+	},
+};
+
+apiClient.use(errorHandlingMiddleware);
