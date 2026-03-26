a8f4496fabd0d970d3c26393efd784d7aa1453ed
Bart Mikolajczuk <bart.ext@zenml.io>
2026-03-26 11:21:35 +0100

    style: make visualization skeleton smaller

--- src/modules/checkpoints/ui/VisualizationSkeleton.tsx
+++ src/modules/checkpoints/ui/VisualizationSkeleton.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from "@/shared/ui/skeleton";
 
 export function VisualizationSkeleton() {
 	return (
-		<div className="flex flex-col gap-3 p-4">
+		<div className="flex flex-col gap-2 p-4">
 			<Skeleton className="h-4 w-3/4" />
 			<Skeleton className="h-4 w-1/2" />
-			<Skeleton className="h-32 w-full" />
+			<Skeleton className="h-16 w-2/3" />
 		</div>
 	);
 }
