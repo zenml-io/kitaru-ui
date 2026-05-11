19af21dd95d09fafd152cc74d25614be3fc7d69d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-05-11 12:18:54 +0200

    refactor: remove memory (#190)

--- src/modules/memory/domain/fetch-namespace-memories.ts
+++
@@ -1,8 +0,0 @@
-import { MEMORY_TAG_SCOPE_TYPE_PREFIX, type MemoryEntry } from "./memory";
-import { fetchMemoryEntries } from "./fetch-memory-entries";
-
-export function fetchNamespaceMemories(): Promise<MemoryEntry[]> {
-	return fetchMemoryEntries({
-		extraTags: [`${MEMORY_TAG_SCOPE_TYPE_PREFIX}namespace`],
-	});
-}
